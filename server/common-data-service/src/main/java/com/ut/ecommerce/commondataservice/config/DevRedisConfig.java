ff94de79fd6074d1104ca2ffc9e22d2713036751
Christoooophe <christophealfier02@gmail.com>
2025-05-08 18:22:50 +0200

    first commit

---
+++ server/common-data-service/src/main/java/com/ut/ecommerce/commondataservice/config/DevRedisConfig.java
@@ -0,0 +1,38 @@
+package com.ut.ecommerce.commondataservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Objects;
+
+@Configuration
+@Profile("dev")
+public class DevRedisConfig {
+
+    @Autowired
+    Environment environment;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        System.out.println("Loading Dev profile redis config....");
+        RedisStandaloneConfiguration redisStandaloneConfiguration =
+                new RedisStandaloneConfiguration("redis-container", 6379);
+
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(""));
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
